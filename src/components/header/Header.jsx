2dd6df4883e5b05816887eab283b9d86d7da5682
MathieuGAUBAL <b_jayson@hotmail.com>
2022-04-09 22:19:44 +0200

    responsive tablet ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,16 +11,28 @@ const Header = () => {
                 <b> business.</b>
             </h1>
         </div>
-        <div className="container-header-text">
-        Our marketing and sales automations help you scale your outreach to get more leads for your company.
-        </div>
-        <div className="container-header-btn">
-            <Buttons text="Request Beta Access" cln={"btn-header"}/>
-        </div>
         
-        <div className="container-header-img">
-            <img src="/assets/image-hero-landscape@2x.webp" alt="phone assets"/>
+        <div className="container-part1">
+            <div className="container-part-with-img">
+
+                <div className="container-header-text">
+                Our marketing and sales automations help you scale your outreach to get more leads for your company.
+                </div>
+                <div className="container-header-btn">
+                    <Buttons text="Request Beta Access" cln={"btn-header"}/>
+                </div>
+            </div>
+
+            
+            <div className="container-header-img">
+                <img src="/assets/image-hero-landscape@2x.webp" alt="phone assets"/>
+            </div>
+            <div className="container-header-img-2">
+                <img src="/assets/image-hero-portrait@2x.webp" alt="phone assets"/>
+            </div>
         </div>
+
+
         <div className="container-header-stats">
             <div className="stat1">
                 <p>2K+</p>
